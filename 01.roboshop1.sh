7012f1b180161e5000d8a098e4e38489aabe569a
Krish <msk972645@gmail.com>
2026-05-14 18:53:37 +0530

    shell-roboshop

--- 01.roboshop1.sh
+++ 01.roboshop1.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0cf96f7c7fcb5a583 --query 'Instances[0].InstanceId')
-PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].[InstanceID,PublicIpAddress]')
-PRIVATE_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].[InstanceID,PrivateIpAddress]')
+AMI_ID=ami-0220d79f3f480ecf5
+SG_ID=sg-0cf96f7c7fcb5a583
+HOSTED_ZONE=Z07452733M0KKP6J4JY7P
+DOMAIN_NAME=msk-dps.xyz
 
-for server in "$@"
+for instance in "$@"
 do
-	if [ $frontend -ne 0 ]; then
-		echo "Private Ip is: $PRIVATE_IP"
-	else
-		echo "Public IP is: $PUBLIC_IP"
+	INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+	if [ $instance != "frontend" ]; then
+		IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].[InstanceId,PrivateIpAddress]")
+		echo "$instance: $IP"
+    else
+		IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].[InstanceId,PublicIpAddress]")
+		echo "$instance: $IP"
 	fi
 done
