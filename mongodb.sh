2afe5fe62dc3cd5afddf2af229b099dddb077e50
Krish <msk972645@gmail.com>
2026-05-16 18:28:26 +0530

    shell-roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -10,9 +10,12 @@ N="\e[0m"
 LOG_FOLDER="/var/log/mongo_logs"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
+END_TIME=$(date +%s)
+TIME_TAKEN=$(($END_TIME - $START_TIME))
 
 mkdir -p $LOG_FOLDER
-echo "script started excecuting at: $(date)" | tee -a $LOG_FILE
+echo "script started excecuting at: $START_TIME" | tee -a $LOG_FILE
 
 if [ $USER_ID -ne 0 ]; then
 	echo "error: this script can be executed with root access"
@@ -50,6 +53,5 @@ VALIDATE $? "restarted mongodb" | tee - a $LOG_FILE
 
 netstat -lntp | tee - a $LOG_FILE
 
-echo "script ended excecuting at: $(date)" | tee -a $LOG_FILE
-
-
+echo "script ended excecuting at: $END_TIME" | tee -a $LOG_FILE
+echo "total time taken to execute the script: $TIME_TAKEN seconds" | tee -a $LOG_FILE
