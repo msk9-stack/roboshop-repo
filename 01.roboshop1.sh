7f6f08740be91acbe6d77eee32558e5e73e4599d
Krish <msk972645@gmail.com>
2026-05-16 18:23:00 +0530

    shell-roboshop

--- 01.roboshop1.sh
+++ 01.roboshop1.sh
@@ -7,35 +7,61 @@ DOMAIN_NAME=msk-dps.xyz
 
 for instance in "$@"
 do
-	INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
-	if [ $instance != "frontend" ]; then
-		IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].[PublicIpAddress,PrivateIpAddress]' --output text)
-		RECORD_NAME="$instance.$DOMAIN_NAME"
-		echo "Route53: $instance.$DOMAIN_NAME"
-	else
-		IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-		RECORD_NAME="$DOMAIN_NAME"
-		echo "Route53: $DOMAIN_NAME"
-	fi
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text)
 
-	echo "$instance: $IP"
+    PUBLIC_IP=$(aws ec2 describe-instances \
+    --instance-ids $INSTANCE_ID \
+    --query 'Reservations[0].Instances[0].PublicIpAddress' \
+    --output text)
 
-	aws route53 change-resource-record-sets \
+    PRIVATE_IP=$(aws ec2 describe-instances \
+    --instance-ids $INSTANCE_ID \
+    --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+    --output text)
+
+    if [ "$instance" != "frontend" ]; then
+
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+
+        echo "Instance Name : $instance"
+        echo "Public IP    : $PUBLIC_IP"
+        echo "Private IP   : $PRIVATE_IP"
+        echo "Route53      : $RECORD_NAME"
+        echo
+		IP=$PRIVATE_IP
+
+    else
+
+        RECORD_NAME="$DOMAIN_NAME"
+
+        echo "Instance Name : $instance"
+        echo "Public IP    : $PUBLIC_IP"
+        echo "Route53      : $RECORD_NAME"
+        echo
+		IP=$PUBLIC_IP
+    fi
+
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $HOSTED_ZONE \
     --change-batch '
     {
-        "Comment": "Updating record set"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
-            }]
-        }
+        "Comment": "Updating record set",
+        "Changes": [{
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [{
+                    "Value": "'$IP'"
+                }]
+            }
         }]
-    }
-    '
+    }'
 done
