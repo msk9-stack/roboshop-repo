b9b8d52df5b6a185a1ef614803eec61bc81cf3f9
Krish <msk972645@gmail.com>
2026-05-23 16:21:51 +0530

    shell-roboshop

--- mysql.sh
+++ mysql.sh
@@ -45,5 +45,7 @@ VALIDATE $? "starting mysql"
 mysql_secure_installation --set-root-pass RoboShop@1 &>>LOG_FILE
 VALIDATE $? "setting up the root password for mysql"
 
+sleep 5
+
 echo "script started excecuting at: $(date)" | tee -a $LOG_FILE
 echo "total time taken for script to execute: $TIME_TAKEN sec" | tee -a $LOG_FILE
